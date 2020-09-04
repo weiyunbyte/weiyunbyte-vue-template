16d14b3f29eddd90d5a6837c9af6775aa07c4dde
renwei <weiyunbyte@weiyunbyte.com>
2020-09-04 16:39:51 +0800

    html

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,68 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>{{count}}</p>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <van-cell-group>
-      <van-cell title="单元格" value="内容" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-    </van-cell-group>
-    <van-skeleton title avatar :row="2" :loading="loading">
-      <div><p>实际内容</p></div>
-    </van-skeleton>
     <Nav></Nav>
   </div>
 </template>
 <script>
-/* eslint-disable */
 import Nav from '@/common/nav/nav.vue'
-import { Button, Cell, CellGroup, Skeleton } from 'vant'
 export default {
   name: 'About',
   components: {
-    Nav,
-    VanButton: Button,
-    VanCell: Cell,
-    VanCellGroup: CellGroup,
-    VanSkeleton: Skeleton
+    Nav
   },
   data () {
     return {
-      msg: "about",
-      loading: true
+      msg: "about"
     }
-  },
-  computed: {
-    count() {
-      return this.$store.state.count
-    }
-  },
-  methods:{
-    text(){
-      this.$http
-          .user
-          .index()
-          .then(data => {console.log(data)})
-          .catch(error => {console.log(error)})
-    },
-    increment() {
-      this.$store.commit('increment')
-      console.log(this.$store.state.count)
-    }
-  },
-  mounted(){
-    this.increment()
-    // this.$store.commit('increment')
-    // console.log(this.$store.state)
-
-    this.text()
-    setTimeout(() => {
-      this.loading = false
-    },5000)
   }
 }
 </script>
@@ -72,5 +24,11 @@ export default {
 }
 h1{
   @include test;
+}
+.about {
+  width: 100%;
+  height: auto;
+  min-height: 100%;
+  background: #ccc;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,148 +1,21 @@
 <template>
   <div class="home">
-    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>1</van-swipe-item>
-      <van-swipe-item>2</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
-    </van-swipe>
-    <!-- <van-image fit="contain" width="100" height="100" :src="require('../assets/logo.png')" /> -->
-    <van-row type="flex" justify="center" class="home_box">
-      <van-col class="home_box_list" span="8">
-        <van-image
-         fit="comtain"
-         width="100"
-         height="100"
-         src="https://img.yzcdn.cn/vant/cat.jpeg"
-        />
-      </van-col>
-      <van-col class="home_box_list" span="8" offset="4">span: 8</van-col>
-    </van-row>
-
-    <div></div> 
-    <p>{{msg}}</p>
-    <p>{{count}}</p>
-    <p>{{doneTodosCount}}</p>
-    <p>{{getTodoById(1)}}</p>
-    <p>{{getnames}}</p>
-    <p>{{name}}</p>
-    <p>{{getname}}</p>
-    <p>null</p>
+    <h1>This is an home page</h1>
     <Nav></Nav>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
 import Nav from '@/common/nav/nav.vue'
-import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
-import { Image as VanImage, Col, Row, Swipe, SwipeItem } from 'vant'
 export default {
   name: 'Home',
   components: {
-  	Nav,
-    VanImage: VanImage,
-    VanCol: Col,
-    VanRow: Row,
-    VanSwipe: Swipe,
-    VanSwipeItem: SwipeItem
+    Nav
   },
   data () {
     return {
       msg: "Home"
     }
-  },
-  computed: {
-    ...mapState({
-      count: 'count'
-    }),
-    ...mapState('home', {
-      name: 'name'
-    }),
-    ...mapGetters({
-      doneTodosCount: 'doneTodosCount',
-      getTodoById: 'getTodoById'
-    }),
-    ...mapGetters('a' ,{
-      getnames: 'getnames'
-    }),
-    ...mapGetters('home', {
-      getname: 'getname'
-    })
-  },
-  methods: {
-    // aa() {
-    //   this.$store.commit('increment')
-    // },
-    // ...mapMutations({
-    //   aa: 'increment'
-    // }),
-    // bb() {
-    //   // this.$store.commit('incrementTwo',{num: 11})
-    //   this.$store.commit({
-    //     type: 'incrementTwo',
-    //     num: 13
-    //   })
-    //   this.$store.commit('setTosos')
-    // },
-    ...mapMutations({
-      aa: 'increment',
-      SOME_MUTATION: 'SOME_MUTATION',
-      bb: 'incrementTwo'
-    }),
-    ...mapMutations('home',{
-      setname: 'setname'
-    }),
-    ...mapActions({
-      increment: 'increment',
-      cc: 'incrementTwo',
-      actionA: 'actionA',
-      actionB: 'actionB',
-      actionC: 'actionC',
-      actionD: 'actionD'
-    }),
-    ...mapActions('home',{
-      setname: 'setname',
-      getglomessage: 'getglomessage'
-    })
-  },
-  mounted() {
-    this.aa()
-    this.bb({num: 99})
-    this.SOME_MUTATION()
-    console.log(this.getTodoById(1))
-    // this.$store.dispatch('increment')
-    // this.$store.dispatch({
-    //   type: 'incrementTwo',
-    //   num: 999
-    // })
-    // this.increment()
-    this.cc({num: 888})
-    this.actionB().then(() =>{
-      console.log(1111)
-    }).catch((err) => {
-      console.log(err)
-    })
-    // this.actionC().then(() => {
-    //   console.log(222)
-    // }).catch((err) => {
-    //   console.log(err)
-    // })
-    this.actionD().then(() => {
-      console.log('actionD结束')
-    }).catch((err) => {
-      console.log(err)
-    })
-    ////////////////
-    console.log(this.$store.state.a.name)
-    console.log(this.$store.state.home.name)
-    console.log(this.getname)
-    // this.setname({text:'帅'})
-    this.getglomessage({
-      text: '帅帅'
-    }).then(() =>{
-      console.log('1234567')
-    })
   }
 }
 </script>
@@ -152,26 +25,5 @@ export default {
   height: auto;
   min-height: 100%;
   background: #ccc;
-}
-.logo{
-  width: 100px;
-  height: 100px;
-  margin: 0 auto;
-  img{
-    display: block;
-    width: 100%;
-  }
-}
-.home_box{
-  .home_box_list{
-    background: #39a9ed;
-  }
-}
-.my-swipe .van-swipe-item {
-  color: #fff;
-  font-size: 20px;
-  line-height: 150px;
-  text-align: center;
-  background-color: #39a9ed;
 }
 </style>
